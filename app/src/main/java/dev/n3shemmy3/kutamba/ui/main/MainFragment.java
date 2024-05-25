fd5d9c0020a8a7e31c9981bcae4355d0df866e73
Shemmy <shemmynyirenda3@gmail.com>
2024-05-25 21:13:28 +0200

    Move to a simpler and more minimal main fragment
    move from viewpager to single page

--- app/src/main/java/dev/n3shemmy3/kutamba/ui/main/MainFragment.java
+++ app/src/main/java/dev/n3shemmy3/kutamba/ui/main/MainFragment.java
@@ -2,69 +2,41 @@ package dev.n3shemmy3.kutamba.ui.main;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.imageview.ShapeableImageView;
 
 import dev.n3shemmy3.kutamba.R;
-import dev.n3shemmy3.kutamba.ui.base.BaseFragment;
+import dev.n3shemmy3.kutamba.ui.base.AppFragment;
 
-public class MainFragment extends BaseFragment implements NavigationBarView.OnItemSelectedListener {
+public class MainFragment extends AppFragment implements Toolbar.OnMenuItemClickListener {
 
 
-    private ViewPager2 pager;
-    private BottomNavigationView nav;
-    private MainPagerAdapter adapter;
+    private ShapeableImageView avatar;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
-        pager = root.findViewById(R.id.viewPager);
-        nav = root.findViewById(R.id.nav);
-
-        adapter = new MainPagerAdapter(getChildFragmentManager(), getLifecycle());
-        pager.setAdapter(adapter);
-        pager.setUserInputEnabled(false);
-        pager.setOffscreenPageLimit(4);
-        nav.setOnItemSelectedListener(this);
-        return root;
+    public int getLayoutId() {
+        return R.layout.fragment_main;
     }
 
     @Override
-    public boolean onBackPressed() {
-        if (nav.getSelectedItemId() != R.id.mainFragemnt) {
-            nav.setSelectedItemId(R.id.mainFragemnt);
-            return true;
-        }
-        return super.onBackPressed();
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        toolbar.setOnMenuItemClickListener(this);
+        avatar = toolbar.getMenu().findItem(R.id.actionMenu).getActionView().findViewById(R.id.avatar);
+        avatar.setOnClickListener(v -> navigate(R.id.openDetails));
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        item.setChecked(true);
+    public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.mainFragemnt:
-                pager.setCurrentItem(0, false);
-                break;
-            case R.id.historyFragment:
-                pager.setCurrentItem(1, false);
-                break;
-            case R.id.browseFragment:
-                pager.setCurrentItem(2, false);
-                break;
-            case R.id.moreFragment:
-                pager.setCurrentItem(3, false);
-                break;
+            case R.id.actionSearch -> navigate(R.id.openSearch);
+            case R.id.actionMenu -> navigate(R.id.openDetails);
         }
         return true;
     }
--- app/src/main/java/dev/n3shemmy3/kutamba/ui/search/SearchFragment.java
+++ app/src/main/java/dev/n3shemmy3/kutamba/ui/search/SearchFragment.java
@@ -1,20 +1,117 @@
 package dev.n3shemmy3.kutamba.ui.search;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import dev.n3shemmy3.kutamba.R;
 import dev.n3shemmy3.kutamba.ui.base.AppFragment;
 
-public class SearchFragment extends AppFragment {
+public class SearchFragment extends AppFragment implements TextWatcher, View.OnClickListener {
+
+    private EditText searchView;
+    private Button searchButton;
+    private Button clearButton;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_search;
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        searchView = view.findViewById(R.id.searchView);
+        searchButton = view.findViewById(R.id.searchButton);
+        clearButton = view.findViewById(R.id.clearButton);
+
         toolbar.setTitle(R.string.label_search);
         toolbar.setNavigationIcon(R.drawable.outline_arrow_back_24);
-        toolbar.setOnClickListener(v -> navigate(R.id.openSearch));
+        toolbar.setNavigationOnClickListener(v -> {
+            if (searchView.isEnabled())
+                searchView.setEnabled(false);
+            else
+                navigateUp();
+        });
+
+        /*
+        for some reason the EditText inflates and gets enabled
+        which breaks the back navigation
+         */
+        searchView.setEnabled(false);
+        searchView.addTextChangedListener(this);
+        searchView.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                searchView.setEnabled(false);
+                return true;
+            }
+            return false;
+        });
+        searchView.setOnFocusChangeListener((v, hasFocus) -> {
+            InputMethodManager imm = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            searchButton.setVisibility(hasFocus ? View.GONE : View.VISIBLE);
+            clearButton.setVisibility(hasFocus ? View.VISIBLE : View.GONE);
+            if (hasFocus)
+                imm.showSoftInput(searchView, InputMethodManager.SHOW_FORCED);
+            else
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+        });
+        clearButton.setOnClickListener(this);
+        searchButton.setOnClickListener(this);
+
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (searchView.isEnabled()) {
+                    searchView.setEnabled(false);
+                    return;
+                }
+                navigateUp();
+            }
+        });
     }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.clearButton:
+                searchView.setText("".trim());
+                break;
+            case R.id.searchButton:
+                searchView.setEnabled(!searchView.isEnabled());
+                if (searchView.isEnabled()) {
+                    searchView.requestFocus();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        clearButton.setVisibility(s.length() > 3 ? View.VISIBLE : View.GONE);
+    }
+
+
 }
