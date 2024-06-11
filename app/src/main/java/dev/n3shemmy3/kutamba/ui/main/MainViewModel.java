858180d2573d1810c1885a2b66998ff5dcf4531b
Shemmy <shemmynyirenda3@gmail.com>
2024-06-12 01:16:18 +0200

    Add retrofit for api calls

---
+++ app/src/main/java/dev/n3shemmy3/kutamba/ui/main/MainViewModel.java
@@ -0,0 +1,17 @@
+package dev.n3shemmy3.kutamba.ui.main;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainViewModel extends AndroidViewModel {
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void getLatestMedia() {
+
+    }
+}
